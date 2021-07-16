af8497b2916862b7c4bea03f208aea71e3e6a89f
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-07-15 22:47:44 -0300

    feat: cofigurando e finalizando modulo redux na prática

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import {createStore} from "redux";
+import { ICartState } from "./modules/cart/types";
+import rootReducer from "./modules/rootReducer";
+
+import {composeWithDevTools} from "redux-devtools-extension";
+
+export interface IState {
+  cart: ICartState;
+}
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
